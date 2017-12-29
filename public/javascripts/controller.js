971356191daff4168a9d57accb58d0e5ab2a4142
Mishal Shah <shahmishal1998@gmail.com>
2017-12-30 00:22:25 +0530

    query working properly

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -7,15 +7,21 @@ app.controller('AppCtrl',['$scope','$http',function ($scope,$http) {
     $scope.hdt = false;
     $scope.qry = [];
     $scope.query = function () {
-
+        var d_qstdt = $scope.qstdt.split("/");
+        var fd_qstdt = d_qstdt[2]+d_qstdt[0]+d_qstdt[1];
+        var d_qedt = $scope.qedt.split("/");
+        var fd_qedt = d_qedt[2]+d_qedt[0]+d_qedt[1];
         for(var i=0;i<$scope.fdlist.length;i++)
         {
-            if($scope.fd.dtpr <= $scope.qstdt && $scope.fd.dtmt>=$scope.qedt)
+
+            if($scope.fdlist[i].fd_dtpr >= fd_qstdt && $scope.fdlist[i].fd_dtmt<= fd_qedt)
             {
+                //console.log("HERE");
                 $scope.qry[i]=true;
             }
             else
             {
+                //console.log("false HERE");
                 $scope.qry[i]=false;
             }
             /*console.log($scope.fdlist[i]);*/
@@ -41,6 +47,14 @@ app.controller('AppCtrl',['$scope','$http',function ($scope,$http) {
     refresh();
     $scope.addfd = function () {
         console.log($scope.fd);
+        console.log($scope.fd.dtpr);
+        console.log($scope.fd.dtmt);
+        var d_dtpr = $scope.fd.dtpr.split("/");
+        $scope.fd.fd_dtpr = d_dtpr[2]+d_dtpr[0]+d_dtpr[1];
+        console.log($scope.fd.fd_dtpr);
+        var d_dtmt = $scope.fd.dtmt.split("/");
+        $scope.fd.fd_dtmt = d_dtmt[2]+d_dtmt[0]+d_dtmt[1];
+        console.log($scope.fd.fd_dtmt);
         $scope.fd.gain = (parseInt($scope.fd.amtmt) || 0) - (parseInt($scope.fd.amtpr)||0);
 
         console.log($scope.fd.gain);
